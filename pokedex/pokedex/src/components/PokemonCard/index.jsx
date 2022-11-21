5570308f2771c51182e3a14b8c743294afad93c0
marcelovenagui <marcelovenancioaguiar@gmail.com>
2022-11-21 20:30:14 -0300

    Continuando o site

--- pokedex/pokedex/src/components/PokemonCard/index.jsx
+++ pokedex/pokedex/src/components/PokemonCard/index.jsx
@@ -5,9 +5,9 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
-export default function MultiActionAreaCard() {
+export default function Navbar() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345, }}>
       <CardActionArea>
         <CardMedia
           component="img"
@@ -19,17 +19,17 @@ export default function MultiActionAreaCard() {
           <Typography gutterBottom variant="h5" component="div">
             Lizard
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          {/*<Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
-          </Typography>
+            </Typography>*/}
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      {/*<CardActions>
         <Button size="small" color="primary">
           Share
         </Button>
-      </CardActions>
+          </CardActions>*/}
     </Card>
   );
 }
--- pokedex/pokedex/src/pages/Home.jsx
+++ pokedex/pokedex/src/pages/Home.jsx
@@ -1,10 +1,34 @@
+import {Grid} from '@mui/material';
 import React from 'react';
 import Navbar from '../components/Navbar';
+import PokemonCard from '../components/PokemonCard'
+import { Container } from '@mui/system';
+import axios from 'axios'
+
 
 const Home = () => {
+  axios.get('https://pokeapi.co/api/v2/')
   return (
     <div>
       <Navbar />
+      <Container maxWidth='xg'>
+        <Grid container>
+            <Grid item xs={3}>
+                <PokemonCard />
+            </Grid>
+            <Grid item xs={3}>
+                <PokemonCard />
+            </Grid>
+            <Grid item xs={3}>
+                <PokemonCard />
+            </Grid>
+            <Grid item xs={3}>
+                <PokemonCard />
+            </Grid>
+        </Grid>
+      </Container>
+      
+      
     </div>
   )
 }
